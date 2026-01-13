d50d5550df684a042fef15e46c0a1f2735e1aa38
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-13 19:01:43 +0530

    disable firebase for now

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Root from './Root.tsx'
-import { initializeFirebase } from './lib/firebase'
+import App from './App.tsx'
+// import { initializeFirebase } from './lib/firebase'
 
-const firebaseApp = initializeFirebase()
-console.log('[firebase] initialized app', firebaseApp.name)
+// const firebaseApp = initializeFirebase()
+// console.log('[firebase] initialized app', firebaseApp.name)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>,
 )

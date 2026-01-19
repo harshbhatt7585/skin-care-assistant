dec588cc8fbf6cfbd1e80bfa59c63f00c6e7f335
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-19 17:33:15 +0530

    search api

--- backend/routers/search.py
+++ backend/routers/search.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
+from schema.search import SearchVectorDBRequest, SearchVectorDBResponse
 
 search_router = APIRouter(prefix="/search", tags=["search"])
 
 
-@search_router.get("/search")
-async def search(query: str):
-    return {"message": "Hello, World!"}
+@search_router.post("/search-vector-db")
+async def search_vector_db(payload: SearchVectorDBRequest):
+    query = payload.query
+    uid = payload.uid
+    timestamp = payload.timestamp
+
+    results = search_vector_db(query, uid, timestamp)
+    return SearchVectorDBResponse(results=results)

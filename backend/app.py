537f6dc62db35e91786affcfe4e6359f06768871
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-19 18:37:36 +0530

    fix deprecation stuff

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,19 @@
+from contextlib import asynccontextmanager
+
 import fastapi
 
 from database.firebase import init_firebase
 from routers.auth import auth_router
 from routers.search import search_router
 
-app = fastapi.FastAPI()
 
-
-@app.on_event("startup")
-def startup_event() -> None:
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
     init_firebase()
+    yield
 
+
+app = fastapi.FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(search_router)

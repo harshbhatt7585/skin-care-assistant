5424a6da8f3f61c5fe1e555481719b2bc3685c49
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-01-20 17:26:02 +0530

    be more specific with the user types

--- backend/schema/auth.py
+++ backend/schema/auth.py
@@ -6,14 +6,14 @@ class UserPersonal(BaseModel):
     email: str
     name: str
     uid: str
-    gender: str
-    country: str
+    gender: str | None = None
+    country: str | None = None
 
 
 class User(BaseModel):
     personal: UserPersonal
-    last_scanned: datetime
-    last_chat: datetime
+    last_scanned: datetime | None = None
+    last_chat: datetime | None = None
     created_at: datetime
 
 

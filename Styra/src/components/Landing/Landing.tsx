a4b1b9ae36ea21a1db100d95d4b672d1d6e08bdc
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-02-16 23:53:52 +0530

    download images to load faster

--- Styra/src/components/Landing/Landing.tsx
+++ Styra/src/components/Landing/Landing.tsx
@@ -16,13 +16,13 @@ const CATEGORY_SPOTLIGHT = [
 ]
 
 const SHOWCASE_IMAGES = [
-  'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS1N1Q1ob759rAeeIsCgmDkhcttNqyYWQZ6LQPUrgDLwUCGX0aDiQbwvsjuFy1dt6f_pLOBgaZD-9E9cRQ0CpWtVU9IvqlziXs0bjnn3uGq6YcR8DFgkoGZLzKhczOkS0pWxODPRw&usqp=CAc',
-  'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcdniGOmosttFIVjpUgE23p3V2alMUsC1nIWecMUkIM8gwAWoSCFljl7WFet41VkuB0indkiDA7iq3X4Yza0ohSgN-U3nXIw2XpfORc6_Sri9sKBcdy8rctn-Iw2_6pgMSLqk3134&usqp=CAc',
-  'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ6AHbrPBB0quvnfTIB3in2PEY9D-kQXx87hOa3fpWtkZ6G8APpqHmKBCgzNSt2qNs6cLVrlyR0RhQIfLcWgku64XBL6f3EuJPiXyHbNsorfb08EKhHMRKI3Z3pREvK_MdwTO8m5VI&usqp=CAc',
-  'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQdoNa28ng_qRZgFNXn5aLed69zE-cOs9S4Q3XSoAlAOhyIHk5DSWS_z8dY4HOn0UPxJGh_IO3Efgevt7xLyXq3RzSmvoELRXa-OmatVZPo',
-  'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT4cT0t3vN0qbUe7GJesqussspk5jHgHLwDj5dL6MSmsO_E2zbxoocVMzwctp1TPxOU3ASLNbaNV01uCZMCUSkBRhwr8zajq8IrJYyAebqyBsb6F4HdTG3Vtw',
-  'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTfq8xkQT54ldeImiosxgYeCTYVXPfRnlDMb5V9CQHztY8OYdzNOnBIYCqo4yJT_tLBWAW9ZMdXFjXVfsr9AXUCptpf4bxyD0ncSKrB9cg2nW8Fr2dcYRPt3A',
-  'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTip__xuCzny_EmlrGG5u7ML1SS1SmOu362ns0wIc_CH5ZwU1G8adSTLLCefze1jPINxUtiQvlKAOIF4h_oFKDZOe0VshGT7zrlhiIvMGJzCNlXkmd9TBxTsQU',
+  '/images/lookbook/gshop-1.jpg',
+  '/images/lookbook/gshop-2.jpg',
+  '/images/lookbook/gshop-3.jpg',
+  '/images/lookbook/gshop-4.jpg',
+  '/images/lookbook/gshop-5.jpg',
+  '/images/lookbook/gshop-6.jpg',
+  '/images/lookbook/gshop-7.jpg',
 ]
 
 const Landing = ({ user }: LandingProps) => {
